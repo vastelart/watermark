b6f3afee5f30afd375b36cfe2aa850478c04d069
serger777 <serger777@gmail.com>
2015-11-08 17:17:20 +0400

    создал условие для tile и single

--- app/tests/Sergey/index.php
+++ app/tests/Sergey/index.php
@@ -23,6 +23,8 @@ unset($_SESSION['message']);
     <input type="file" class="fileupload" data-upload-type="image" name="image" multiple>
          </span>
     <input type="file" class="fileupload" data-upload-type="watermark" name="watermark" multiple>
+    <input type="radio" name="action" value="single">
+    <input type="radio" name="action" value="tile">
     <button type="submit" class="btn btn-primary start">
 
     <input type="submit"/>
@@ -34,23 +36,7 @@ unset($_SESSION['message']);
 
 </form>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="File-Upload/js/vendor/jquery.ui.widget.js"></script>
-<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
-<!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="File-Upload/js/jquery.iframe-transport.js"></script>
-<!-- The basic File Upload plugin -->
-<script src="File-Upload/js/jquery.fileupload.js"></script>
-<!-- The File Upload processing plugin -->
-<script src="File-Upload/js/jquery.fileupload-process.js"></script>
-<!-- The File Upload image preview & resize plugin -->
-<script src="File-Upload/js/jquery.fileupload-image.js"></script>
+
 
 
 
