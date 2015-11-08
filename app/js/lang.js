866fea5262991f831d4bc1ea4c282e6005cf1d46
Roman Spazm <troyzemmann@gmail.com>
2015-11-08 04:04:31 +0300

    LANG CHANGE MODULE AND SOME ADDITIONS

--- app/js/lang.js
+++ app/js/lang.js
@@ -4,25 +4,68 @@ var langModule = (function () {
 	var init = _listener;
 
 	//Переменные среды
-	//...
+	var rus = $('#rus', '.languages__list');
+	var eng = $('#eng', '.languages__list');
+	var langPath = './lang.json';
 
 	//Слушаем языки по методике Илоны Давыдовой
 	function _listener() {
-		$(window).load(_getLang);
+		rus.on('click', _getLang);
+		eng.on('click', _getLang);
 	}
 
-	function _getLang() {
-		var xhr = new XMLHttpRequest();
+	function _getLang(event) {
 
-		xhr.open('GET', 'js/lang.json', true);
-		xhr.send();
+		//Отменяем событие бай дефолт и вешаем активный класс
+		event.preventDefault();
+		$('.languages__link_active', '.languages__list').removeClass('languages__link_active');
+		$(this).addClass('languages__link_active');
 
-		if (xhr.status === 200) {
-			// вывести результат
-			console.log(xhr.responseText); // responseText -- текст ответа.
-		} else {
-			// обработать ошибку
-			console.log(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
+		//Новый, прекрасный нативный XMLHttpRequest
+		var lang = new XMLHttpRequest();
+		
+		//Нужный нам язык
+		var langToSet = $(this).attr('id');
+
+		lang.open('GET', langPath, true);
+
+		lang.send(null);
+
+		lang.onreadystatechange = function () {
+			if (lang.readyState === 4 && lang.status === 200) {
+				// Сменить язык
+				//console.log(lang.responseText);
+
+				//Парсим модный JSON
+				var langResponse = JSON.parse(lang.responseText);
+
+				//Объявляем переменную языка - будущего номера объекта в массиве файла lang.json
+				var langInFile;
+
+				//Определяем номер объекта языка в массиве файла lang.json
+				switch(langToSet) {
+					case 'rus':
+						langInFile = 0
+						break
+					case 'eng':
+						langInFile = 1
+						break
+				}
+
+				//Устанавливаем язык
+				_setLang(langResponse[langInFile]);
+
+			} else {
+				// Выдать в консоль полный крах и молча наблюдать дальше
+				console.log('FAIL'); //он все равно выводит в консоль FAIL, причем дважды. даже при удачном запросе. подумать.
+			}
+		}
+	}
+
+	function _setLang(langSet) {
+		for(var lang in langSet) {
+			var el = document.querySelector('.' + lang + '');
+			el.textContent = langSet[lang];
 		}
 	}
 
